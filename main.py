95aa6165219c8b949a8c718d57e13640be362563
deckstirK <160159040+deckstirK@users.noreply.github.com>
2024-02-15 08:58:20 -0800

    Hello Upload!

---
+++ main.py
@@ -0,0 +1,114 @@
+#This file was created by: Dexter Kunimura
+
+#import libraries and modules
+import pygame as pg
+from random import randint
+from settings import *
+from sprites import *
+import sys
+from os import path
+
+#define game class
+class Game:
+    #create init functions
+    def __init__(self):
+        #initialize pygame
+        pg.init()
+        #sets the size and name of the game window
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        #get pygame clock and start running
+        self.clock = pg.time.Clock()
+        self.load_data()
+
+                # TRANSPLANT THIS
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        # 'r'     open for reading (default)
+        # 'w'     open for writing, truncating the file first
+        # 'x'     open for exclusive creation, failing if the file already exists
+        # 'a'     open for writing, appending to the end of the file if it exists
+        # 'b'     binary mode
+        # 't'     text mode (default)
+        # '+'     open a disk file for updating (reading and writing)
+        # 'U'     universal newlines mode (deprecated)
+        # below opens file for reading in text mode
+        # with 
+        '''
+        The with statement is a context manager in Python. 
+        It is used to ensure that a resource is properly closed or released 
+        after it is used. This can help to prevent errors and leaks.
+        '''
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                print(line)
+                self.map_data.append(line)
+   # Create run method which runs the whole GAME
+    def new(self):
+        print("create new game...")
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        # self.player1 = Player(self, 1, 1)
+        # for x in range(10, 20):
+        #     Wall(self, x, 5)
+        for row, tiles in enumerate(self.map_data):
+            print(row)
+            for col, tile in enumerate(tiles):
+                print(col)
+                if tile == '1':
+                    print("a wall at", row, col)
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+
+    def update(self):
+        self.all_sprites.update()
+
+    def run(self):
+        # 
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+    def quit(self):
+         pg.quit()
+         sys.exit()
+
+    def draw_grid(self):
+     for x in range (0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x,0), (x, HEIGHT))
+     for y in range (0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0,y), (WIDTH, y))
+
+    def draw(self):
+         self.screen.fill(BGCOLOR)
+         self.draw_grid()
+         self.all_sprites.draw(self.screen)
+         pg.display.flip()
+     
+    def events(self):
+         for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+    #         if event.type == pg.KEYDOWN:
+    #             if event.key == pg.K_LEFT:
+    #                 self.player.move(dx=-1)
+    #             if event.key == pg.K_RIGHT:
+    #                 self.player.move(dx=1)
+    #             if event.key == pg.K_DOWN:
+    #                 self.player.move(dy=1)
+    #             if event.key == pg.K_UP:
+    #                 self.player.move(dy=-1)
+             
+
+#INSTANTIATING!!!! (creating an instance of the game)
+g = Game()
+#use game method run to run the game
+#g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    #g.show_go_screen()
---
+++ settings.py
@@ -0,0 +1,18 @@
+WIDTH = 1024
+HEIGHT = 768
+
+FPS = 30
+
+TITLE = "My Amazing Game"
+
+TILESIZE = 32
+
+BGCOLOR = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+LIGHTGREY = (75, 75, 75)
+BLUE = (0,0,255)
+
+#player settings
+
+PLAYER_SPEED = 300
