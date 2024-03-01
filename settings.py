111cf031b0f8d0ef00691662a73908aec5233055
deckstirK <160159040+deckstirK@users.noreply.github.com>
2024-03-01 09:54:20 -0800

    moved over all code from game_engine

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 WIDTH = 1024
 HEIGHT = 768
 
-FPS = 30
+FPS = 100000000000
 
 TITLE = "My Amazing Game"
 
@@ -12,6 +12,7 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 LIGHTGREY = (75, 75, 75)
 BLUE = (0,0,255)
+YELLOW = (255,255,0)
 
 #player settings
 
