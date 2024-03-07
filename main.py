639183296bec8a2305ab74d74458ed558a6e1a6f
deckstirK <160159040+deckstirK@users.noreply.github.com>
2024-03-07 14:28:23 -0800

    added code for sprite screen in main

--- main.py
+++ main.py
@@ -115,7 +115,25 @@ class Game:
     #                 self.player.move(dy=1)
     #             if event.key == pg.K_UP:
     #                 self.player.move(dy=-1)
-             
+    def show_start_screen(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_text(self.screen, "This is the start screen", 24, WHITE, WIDTH/2 - 32, 2)
+        pg.display.flip()
+        self.wait_for_key()
+        
+        pass 
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting: 
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting == False
+                    self.quit()
+                if event.type == g.KEYUP:
+                    waiting == False
+                    # self.running = False
 
 #INSTANTIATING!!!! (creating an instance of the game)
 g = Game()
--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ RED = (255, 0, 0)
 LIGHTGREY = (75, 75, 75)
 BLUE = (0,0,255)
 YELLOW = (255,255,0)
+WHITE = (255,255,255)
 
 
 #player settings
